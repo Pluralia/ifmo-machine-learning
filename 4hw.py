38496cfa333cbb8fbe5207d60bf0aed327f2d61b
Irina Pluralia <irina@metrix.ai>
2019-03-17 03:32:24 +0300

    add figures

--- 4hw.py
+++ 4hw.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import decision_tree as dt
 import roc
@@ -8,17 +9,31 @@ import roc
 def get_best_tree(impurity, data, labels):
     train_data, test_data, train_labels, test_labels = train_test_split(data, labels, train_size=0.8, test_size=0.2)
 
+    max_max_deps = 10
+    accuracy_list = np.empty(max_max_deps)
     res_tree = None
     res_accuracy = 0
-    for max_depth in range(1, 11):
+    for max_depth in range(1, max_max_deps + 1):
+        print(max_depth)
         tree = dt.DecisionTree(impurity, max_depth).build(train_data, train_labels)
         new_labels = tree.predict(test_data)
         accuracy = np.mean(new_labels == test_labels)
         if res_accuracy < accuracy:
             res_tree = tree
             res_accuracy = accuracy
+        accuracy_list[max_depth - 1] = accuracy
+
+    plt.figure()
+    plt.plot(np.arange(max_max_deps) + 1, accuracy_list)
+    plt.title(impurity.__name__)
+    plt.xlabel("max_depth")
+    plt.ylabel("accuracy")
+    plt.pause(0.001)
+
     return res_tree
 
+
+plt.ion()
 
 # cancer
 # c_df = pd.read_csv("cancer.csv")
@@ -27,6 +42,7 @@ def get_best_tree(impurity, data, labels):
 # c_data = c_df[:, 1:]
 #
 # for i, impurity in enumerate([dt.misclass_err, dt.entropy, dt.gini_index]):
+#     print("impurity: " + impurity.__name__)
 #     tree = get_best_tree(impurity, c_data, c_labels)
 #     roc.evaluate_best_features(c_data, tree.predict(c_data))
 
@@ -41,5 +57,7 @@ for i, impurity in enumerate([dt.misclass_err, dt.entropy, dt.gini_index]):
     tree = get_best_tree(impurity, s_data, s_labels)
     roc.evaluate_best_features(s_data, tree.predict(s_data))
 
+plt.ioff()
+plt.show()
 
 print("OK")
