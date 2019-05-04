c64277883997c50439d2df29d37cefe75e37cd9d
Irina Pluralia <irina@metrix.ai>
2019-05-04 12:26:38 +0300

    add #2

--- 1hw.py
+++ 1hw.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import roc
-from gaussian_naive_bayes import *
+from naive_bayes import *
 
 
 # spam
@@ -16,9 +16,9 @@ data = data / np.max(data, axis=0)
 tr_data, val_data, tr_labels, val_labels = train_test_split(data, labels, train_size=0.8, test_size=0.2)
 
 
-new_tr_labels, _ = predict(tr_data, tr_labels, tr_data)
+new_tr_labels, _ = gauss_predict(tr_data, tr_labels, tr_data)
 tr_accuracy = np.sum(new_tr_labels == tr_labels) / tr_data.shape[0]
-new_val_labels, features_prob = predict(tr_data, tr_labels, val_data)
+new_val_labels, features_prob = gauss_predict(tr_data, tr_labels, val_data)
 val_accuracy = np.sum(new_val_labels == val_labels) / val_data.shape[0]
 print("Result for train data: ", tr_accuracy)
 print("Result for validate data: ", val_accuracy)
