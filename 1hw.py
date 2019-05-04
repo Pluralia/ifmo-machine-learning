37aeb330cfdacd69e51af8672d26afadfa0d1534
Irina Pluralia <irina@metrix.ai>
2019-05-04 08:00:36 +0300

    add #1

---
+++ 1hw.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import roc
+from gaussian_naive_bayes import *
+
+
+# spam
+name = 'spam'
+df = pd.read_csv("spam.csv")
+df = df.values
+labels = np.array(df[:, -1], int)
+data = df[:, 0:-1]
+data = data / np.max(data, axis=0)
+
+tr_data, val_data, tr_labels, val_labels = train_test_split(data, labels, train_size=0.8, test_size=0.2)
+
+
+new_tr_labels, _ = predict(tr_data, tr_labels, tr_data)
+tr_accuracy = np.mean(new_tr_labels == tr_labels) / tr_data.shape[0]
+new_val_labels, features_prob = predict(tr_data, tr_labels, tr_data)
+val_accuracy = np.mean(new_val_labels == val_labels) / val_data.shape[0]
+print("Result for train data: ", tr_accuracy)
+print("Result for validate data: ", val_accuracy)
+
+roc.evaluate_roc_auc(features_prob, val_labels, "accuracy: %.3f" % val_accuracy)
---
+++ roc.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def roc(data, labels):
+    sorted_index = np.argsort(data)
+
+    labels1 = np.sum(labels)
+    labels0 = labels.shape[0] - labels1
+
+    true_positive = labels1
+    false_positive = labels0
+    tp_list = [true_positive]
+    fp_list = [false_positive]
+    for i, (x, curr_label) in enumerate(zip(data[sorted_index], labels[sorted_index])):
+        if i == 0:
+            prev = x
+        if curr_label == 1:
+            true_positive -= 1
+        if curr_label == 0:
+            false_positive -= 1
+        if prev == x:
+            continue
+        prev = x
+        fp_list = [false_positive] + fp_list
+        tp_list = [true_positive] + tp_list
+    return np.array([0] + tp_list) / labels1, np.array([0] + fp_list) / labels0
+
+
+def roc_auc(data, labels):
+    tp_list, fp_list = roc(data, labels)
+    t_sum = (tp_list[1:] + tp_list[:-1]) / 2
+    f_sub = fp_list[1:] - fp_list[:-1]
+    return np.sum(t_sum * f_sub)
+
+
+def evaluate_best_features(data, labels):
+    features_auc = [(roc_auc(data[:, i], labels), i) for i in range(data.shape[1])]
+    best_features = sorted(features_auc, reverse=True)[:3]
+    fig, axes = plt.subplots(ncols=3, figsize=(12, 4))
+    for i, (auc, feature_idx) in enumerate(best_features):
+        tp_list, fp_list = roc(data[:, feature_idx], labels)
+        axes[i].plot(fp_list, tp_list)
+        axes[i].set_title("feature: %i, roc_auc: %.3f" % (feature_idx, auc))
+    plt.show()
+
+
+def evaluate_roc_auc(feature, labels, title):
+    tp_list, fp_list = roc(feature, labels)
+    plt.figure()
+    plt.plot(fp_list, tp_list)
+    plt.title(title + "\nroc_auc: %.3f" % roc_auc(feature, labels))
+    plt.pause(0.001)
