ed8bc8eeec666f9ac8f4c56ff4bdc7c046ef3423
Irina Pluralia <irina@metrix.ai>
2019-05-05 08:19:15 +0300

    update #3, #4

--- 3-4hw.py
+++ 3-4hw.py
@@ -8,7 +8,7 @@ def r_squared(real_y, predict_y):
     return 1 - np.sum((real_y - predict_y) ** 2) / np.sum((real_y - real_y.mean()) ** 2)
 
 
-power = 6
+power = 11
 
 ########################################################################################################################
 
@@ -31,18 +31,18 @@ X = make_x(X, power)
 x = make_x(x, power)
 
 W = (np.linalg.inv(X.T.dot(X)).dot(X.T)).dot(Y)
-predict = x.dot(W)
-rsquare = r_squared(y, predict)
+rsquare = r_squared(y, x.dot(W))
 print(rsquare)
 
 
 max_rsquare = 0
 max_alpha = 0
 r_square = np.empty(201)
+I = np.eye(power + 1)
 for i in range(201):
     alpha = 0.01 * (i - 100)
-    W_reg = (np.linalg.inv(X.T.dot(X) + alpha * np.eye(power + 1)).dot(X.T)).dot(Y)
-    r_square[i] = r_squared(y, x.dot(W_reg))
+    W_reg = (np.linalg.inv(X.T.dot(X) + alpha * I).dot(X.T)).dot(Y)
+    r_square[i] = r_squared(Y, X.dot(W_reg))
     if r_square[i] < 0:
         r_square[i] = r_square[i - 1]
     if max_rsquare < r_square[i]:
@@ -50,12 +50,13 @@ for i in range(201):
         max_alpha = alpha
 print(max_rsquare, max_alpha)
 
-plt.plot(0.01 * (np.arange(201) - 100), r_square, 'r')
-plt.title(f'noisysine (power={power}): ' + "r-square: %.3f;\n" % rsquare + "regular r-square: %.3f " % max_rsquare + f'(alpha={max_alpha})')
-plt.show()
+W_reg = (np.linalg.inv(X.T.dot(X) + max_alpha * I).dot(X.T)).dot(Y)
+reg_rsquare = r_squared(y, x.dot(W_reg))
+print(reg_rsquare)
 
-alpha = max_alpha
-W_reg = (np.linalg.inv(X.T.dot(X) + alpha * np.eye(power + 1)).dot(X.T)).dot(Y)
+plt.plot(0.01 * (np.arange(201) - 100), r_square, 'r')
+plt.title(f'noisysine (power={power}): ' + "r-square: %.3f;\n" % rsquare + "regular r-square: %.3f " % reg_rsquare + f'(alpha={max_alpha})')
+plt.show()
 
 plt.plot(data, labels, '.')
 plt.plot(data, make_x(data, power).dot(W), 'g')
@@ -75,12 +76,10 @@ def make_x(x, power=1):
 # hydrodynamics
 df = pd.read_csv('hydrodynamics.csv')
 df = df.values.astype(np.float32)
-data = df[:, :6]
+data = make_x(df[:, :6], power)
 labels = df[:, 6]
 
 X, x, Y, y = train_test_split(data, labels, train_size=0.8, test_size=0.2)
-X = make_x(X, power)
-x = make_x(x, power)
 
 W = (np.linalg.inv(X.T.dot(X)).dot(X.T)).dot(Y)
 rsquare = r_squared(y, x.dot(W))
@@ -90,11 +89,11 @@ print(rsquare)
 max_rsquare = 0
 max_alpha = 0
 r_square = np.empty(201)
-I = np.eye(X.shape[1] * power + 1)
+I = np.eye(X.shape[1])
 for i in range(201):
     alpha = 0.01 * (i - 100)
     W_reg = (np.linalg.inv(X.T.dot(X) + alpha * I).dot(X.T)).dot(Y)
-    r_square[i] = r_squared(y, x.dot(W_reg))
+    r_square[i] = r_squared(Y, X.dot(W_reg))
     if r_square[i] < 0:
         r_square[i] = r_square[i - 1]
     if max_rsquare < r_square[i]:
@@ -102,6 +101,10 @@ for i in range(201):
         max_alpha = alpha
 print(max_rsquare, max_alpha)
 
+W_reg = (np.linalg.inv(X.T.dot(X) + max_alpha * I).dot(X.T)).dot(Y)
+reg_rsquare = r_squared(y, x.dot(W_reg))
+print(reg_rsquare)
+
 plt.plot(0.01 * (np.arange(201) - 100), r_square, 'r')
-plt.title(f'hydrodynamics (power={power}): ' + "r-square: %.3f;\n" % rsquare + "regular r-square: %.3f " % max_rsquare + f'(alpha={max_alpha})')
+plt.title(f'hydrodynamics (power={power}): ' + "r-square: %.3f;\n" % rsquare + "regular r-square: %.3f " % reg_rsquare + f'(alpha={max_alpha})')
 plt.show()
